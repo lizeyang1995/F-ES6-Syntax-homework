af3a583d3e2fbbad961c72a13cd2963fc4dd07f6
lizeyang1995 <424011527@qq.com>
2020-11-09 22:08:29 +0800

    [lizeyang] feat: complete practice1 and practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (inputData) => {
+  const [data, column, columnNames, resultData] = [inputData.data, inputData.column, [], []]
+  column.map(iterator => columnNames.push(iterator.name))
+  data.map(iterator => {
+    let formatData = new Object()
+    for (const column in columnNames) {
+      formatData[columnNames[column]] = iterator[column]
+    }
+    resultData.push(formatData)
+  })
+  return resultData
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,14 @@
-const inject
+const inject = (items, sections) => {
+  let itemsObject = new Object()
+  for (const key in items) {
+    itemsObject[key] = items[key]
+  }
+  sections.map(iterator => {
+    const insertIndex = iterator.index
+    const behindItem = itemsObject[insertIndex]
+    const nowIndex = items.indexOf(behindItem)
+    items.splice(nowIndex, 0, iterator.content)
+  })
+  return items
+}
 export { inject };
